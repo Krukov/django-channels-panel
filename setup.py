be9ea5839ed4b543b0b016f60389d78198ab830d
Krukov Dima <glebov.ru@gmail.com>
2016-08-30 22:14:02 +0000

    Bump version 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-channels-panel',
-    version='0.0.2',
+    version='0.0.4',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
